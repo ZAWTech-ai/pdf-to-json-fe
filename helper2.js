a8f16e97c19a89262f71ab7997d2fe1369520225
Waleed Umer <wumer@siammakro.co.th>
2024-01-31 17:44:06 +0500

    fix: shuffle array

--- helper2.js
+++ helper2.js
@@ -297,5 +297,13 @@ const identifyBoxedAnswers = (set) => {
       boxedAnswers += answer + ", ";
     }
   });
-  return boxedAnswers;
+  return shuffleArray(boxedAnswers);
 };
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
